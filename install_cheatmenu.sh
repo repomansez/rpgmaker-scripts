7b1bc1262651057fbb4035f38d7a59e5cf0b60d7
repomansez <repomansez@protonmail.com>
2025-12-22 22:01:46 -0300

    sometimes simpler is better

--- install_cheatmenu.sh
+++ install_cheatmenu.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
 # Installs emerladCoder's cheat menu in Overconfident Exorcist
-
-patch_pluginsjs(){ # This code comes directly from emerladCoder's github
-	echo "Searching for and patching RPGM Plugins.js"
-PATTERN='/];/i ,{"name":"Cheat_Menu","status":true,"description":"","parameters":{}}'
-if [ -f "www/js/plugins.js" ]; then
-	cp -v www/js/plugins.js www/js/plugins.js~ 2>/dev/null &&
-		sed -i "$PATTERN" www/js/plugins.js
-elif [ -f "js/plugins.js" ]; then
-	cp -v js/plugins.js js/plugins.js~ 2>/dev/null &&
-		sed -i "$PATTERN" js/plugins.js
-else
-    echo "No RPGM installation found."
-fi
+detect_rpgmver() {
+	       if [ -d "www" ]; then
+		echo "RPGMaker MV detected"
+		export RPGM_VERSION="MV"
+	elif [ -d "js" ]; then
+		echo "RPGMaker MZ detected"
+		export RPGM_VERSION="MZ"
+	fi
+}
+download_plugins_mv(){
+	curl -L \
+  -o www/js/plugins/Cheat_Menu.css \
+  https://raw.githubusercontent.com/emerladCoder/RPG-Maker-MV-Cheat-Menu-Plugin/refs/heads/master/Cheat_Menu/www/js/plugins/Cheat_Menu.css && \
+curl -L \
+  -o www/js/plugins/Cheat_Menu.js \
+  https://raw.githubusercontent.com/emerladCoder/RPG-Maker-MV-Cheat-Menu-Plugin/refs/heads/master/Cheat_Menu/www/js/plugins/Cheat_Menu.js
 }
 
-download_plugins(){
+download_plugins_mz(){
+	
 	curl -L \
   -o js/plugins/Cheat_Menu.css \
   https://raw.githubusercontent.com/emerladCoder/RPG-Maker-MV-Cheat-Menu-Plugin/refs/heads/master/Cheat_Menu/www/js/plugins/Cheat_Menu.css && \
@@ -25,5 +29,24 @@ curl -L \
   https://raw.githubusercontent.com/emerladCoder/RPG-Maker-MV-Cheat-Menu-Plugin/refs/heads/master/Cheat_Menu/www/js/plugins/Cheat_Menu.js
 }
 
+patch_pluginsjs(){ # This code comes directly from emerladCoder's github
+	echo "Searching for and patching RPGM Plugins.js"
+	#PATTERN='$!N; $s/\n]/,{"name":"Cheat_Menu","status":true,"description":"","parameters":{}}\n]/'
+	PATTERN='s/}}$/}},{"name":"Cheat_Menu","status":true,"description":"","parameters":{}}/'
+	if [ ${RPGM_VERSION} = "MV" ]; then
+		cp -v www/js/plugins.js www/js/plugins.js~ 2>/dev/null &&
+		sed -i "$PATTERN" www/js/plugins.js
+		download_plugins_mv
+	elif [ ${RPGM_VERSION} = "MZ"  ]; then
+		cp -v js/plugins.js js/plugins.js~ 2>/dev/null &&
+		sed -i "$PATTERN" js/plugins.js
+		download_plugins_mz
+	else
+    		echo "No RPGM installation found."
+fi
+}
+
+
+
+detect_rpgmver
 patch_pluginsjs
-download_plugins
