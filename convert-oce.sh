efdc73fda279fac72a81471300454ec140967d45
repomansez <repomansez@protonmail.com>
2025-12-22 22:20:40 -0300

    Add license information on all scripts

--- convert-oce.sh
+++ convert-oce.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
 ## Convert Overconfident Exorcist from Windows to Linux
+
+# Copyleft (C) 2025 repomansez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 export nwjs_version=0.78.1
 export gamedir="${1}"
 export newgamedir="nwjs-sdk-v${nwjs_version}-linux-x64"
